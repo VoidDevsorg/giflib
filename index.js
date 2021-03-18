47c9b42744af6d12ec62d2f45da2afe729df2355
ByQush <taskinnmehmett10@gmail.com>
2021-03-18 11:30:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Thank you for using GifLib!
+
+// We have an official Discord!
+// https://discord.gg/Qdbq2v8FM4
+
+module.exports = require("./src/handler.js");
+
+// Documentation:
+// https://https://github.com/VoidDevsorg/giflib#docs
